1ec64164928c900ebdf372a012e813ca679ecde8
Nagarajan Shanmugam <nagarajan.shanmugam@imaginea.com>
2018-10-31 17:06:15 +0530

    Update Diagrams

--- schema-per-tenant/src/main/java/org/imaginea/multitenancy/Application.java
+++ schema-per-tenant/src/main/java/org/imaginea/multitenancy/Application.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class Application {
+public class  Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
